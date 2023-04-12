e873e36e2f14fcd3189aaa22526b3fc9aadc5a47
JinUk Baek <chiggang@jinukui-macbookpro.local>
2023-04-12 18:32:42 +0900

    middleware 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(cunsumer: MiddlewareConsumer) {
+    cunsumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
